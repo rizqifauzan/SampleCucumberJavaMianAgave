0e7a32c4db16ec46f8dda77d1692fe8ca01e4ba6
Mian Agave <mianagave2020@gmail.com>
2024-12-23 15:41:08 +0700

    "Update CI workflow to fix cucumber report and Chrome setup"

--- src/test/java/stepDef/RunnerTest.java
+++ src/test/java/stepDef/RunnerTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty","html:target/cucumber-reports.html"},
+        plugin = {"pretty", "html:target/cucumber-reports.html"},
         glue = {"stepDef"},
         features = {"src/test/java/features"},
         tags = "@web",
